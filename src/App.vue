adb3f78438ea763bcd25bb264c0f2ab87fa4c31c
rafaaulbrich <rafaelacamilly2018@gmail.com>
2023-05-06 14:58:11 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,63 +4,90 @@ const produtos = [
   {
     id: 1,
     nome: 'Camiseta',
-    preco: 49.9
+    preco: 49.9,
+    quant: 1
   },
   {
     id: 2,
     nome: 'Calça',
-    preco: 99.9
+    preco: 99.9,
+    quant: 1
   },
   {
     id: 3,
     nome: 'Meia',
-    preco: 9.9
+    preco: 9.9,
+    quant: 1
   },
   {
     id: 4,
     nome: 'Sapato',
-    preco: 199.9
+    preco: 199.9, 
+    quant: 1
   },
   {
     id: 5,
     nome: 'Boné',
-    preco: 29.9
+    preco: 29.9,
+    quant: 1
   },
   {
     id: 6,
     nome: 'Óculos',
-    preco: 99.9
+    preco: 99.9,
+    quant: 1
   },
   {
     id: 7,
     nome: 'Relógio',
-    preco: 299.9
+    preco: 299.9,
+    quant: 1
   },
   {
     id: 8,
     nome: 'Bermuda',
-    preco: 79.9
+    preco: 79.9,
+    quant: 1
   },
   {
     id: 9,
     nome: 'Cueca',
-    preco: 19.9
+    preco: 19.9,
+    quant: 1
   },
   {
     id: 10,
     nome: 'Meia',
-    preco: 9.9
+    preco: 9.9,
+    quant: 1
   }
 ]
+
+function addPeca() {
+    quant.value++
+}
+
+function removerPeca() {
+  if(quant > 0) {
+    quant.value--
+  }
+}
 
 
 </script>
 
 <template>
+  <div class="carrinho">
+    <button @click="carrinhoCompras">Carrinho</button>
+  </div>
   <div class="produtos">
     <div v-for="(produto, i) in produtos" :key="i" class="card-produto">
       <h2>{{ produto.nome }}</h2>
       <p> R$ {{ produto.preco.toFixed(2).replace('.',',') }}</p>
+      <p> Quantidade: {{ produto.quant }}</p>
+      <button @click="addPeca">+</button>
+      <button @click="removerPeca">-</button>
+      <button @click="addCarrinho">Adicionar ao carrinho</button>
     </div>
   </div>
 </template>
@@ -70,7 +97,7 @@ const produtos = [
   width: 300px;
   height: 300px;
   border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 
 .produtos {
@@ -79,6 +106,10 @@ const produtos = [
   display: grid;
   grid-template-columns: repeat(4, 10fr);
   grid-auto-rows: 400px;
+}
+
+h2, p {
+  margin-left: 20px;
 }
 
 </style>
